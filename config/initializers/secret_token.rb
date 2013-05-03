50be954c23b00a7ca7235c438ebeee0058dfc92e
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2013-05-03 15:01:55 +0300

    added rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleBooks::Application.config.secret_token = '696d1f3320074f7321d4f8b92bb4593bf3bb56fd922b4d41ec41dc93545b16bf0d11ec4abe97e8ebfd07daedb17baffe7150d5813426ce506993982f925afb68'
